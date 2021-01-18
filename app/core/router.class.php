d759007d5348ce1210492d2919bf3f6a8d965157
unknown <rene.colombaro@hotmail.com>
2021-01-18 06:30:27 -0300

    Modificado a classe de rota para a api de notas, movido backups para storage

--- app/core/router.class.php
+++ app/core/router.class.php
@@ -4,58 +4,53 @@
 	{
 		private $controller = "example";
 		private $method = "index";
-		private $prefix = array();//Prefixo da url, por exemplo sysadm é o prefixo de sysadm/home/index
 		private $params = array();
-		function __construct()
+		
+		function dispatch($table)
 		{
-			//var_dump($_GET);
 			$this->url = $this->parseUrl();
-			$this->setPrefix();
-			
-			if(isset($this->url[0]))
-			{
-				if(file_exists("../app/controllers/". $this->prefix['path'] ."{$this->url[0]}.class.php"))
+			//Percorre a tabela de rotas
+				foreach($table as $key => $row)
 				{
-					$this->controller = $this->url[0];
-					unset($this->url[0]);
+					if(isset($this->url[0]) && $this->url[0] == $row['url'])
+					{
+						$this->controller = $this->url[0];
+						unset($this->url[0]);
+						if(file_exists("app/controllers/"."{$this->controller}.class.php"))
+						{
+							//Cria o controller
+								$this->controller = "App\Controllers\\".$this->controller;
+								$this->controller = new $this->controller();
+							//Cria o controller
+						}
+						else
+						{
+							throw new \Exception("Erro 404, não encontrado", 404);
+						}
+					}
+				}
+			//Percorre a tabela de rotas
+			//Chama o método
+				if(method_exists($this->controller, $this->method))//Verifica o método padrão
+				{
+					//Re arrumando o array
+						if(isset($this->url))
+						{
+							$this->params = array_values($this->url);
+							unset($this->url);
+						}
+					//Re arrumando o array
+					//Chama o método
+						call_user_func_array(array($this->controller,$this->method),$this->params);
+					//Chama o método
 				}
 				else
 				{
 					throw new \Exception("Erro 404, não encontrado", 404);
 				}
-			}
-			
-			//Criando o obj do controller
-			$this->controller = "App\Controllers\\". $this->prefix['namespace'] .$this->controller;
-			$this->controller = new $this->controller();
-			//Criando o obj do controller
-			if(isset($this->url[1]))
-			{
-				if(method_exists($this->controller, $this->url[1]))
-				{
-					$this->method = $this->url[1];
-					unset($this->url[1]);
-				}
-				else
-				{
-					throw new \Exception("Erro 404, não encontrado",404);
-				}
-			}
-			
-			
-			//Re arrumando o array
-			if(isset($this->url))
-			{
-				$this->params = array_values($this->url);
-				unset($this->url);
-			}
-			//Re arrumando o array
-			
-			//Chamando o metodo
-			call_user_func_array(array($this->controller,$this->method),$this->params);
-			//Chamando o metodo
+			//Chama o método
 		}
-		
+
 		function parseUrl()
 		{
 			if(isset($_GET['URL']))
@@ -64,44 +59,6 @@
 				unset($_GET['URL']);
 				return $this->url;
 			}
-		}
-		
-		static function getBaseUrl()
-		{
-			if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
-			{
-				$link = "https"; 
-			}
-			else
-			{
-				$link = "http";
-			}
-			$link .= "://{$_SERVER['HTTP_HOST']}/receitasOn/public/";//caminho para o front controller
-			return $link;
-		}
-		
-		function setPrefix()
-		{
-			//Testa se tem um prefixo
-			if(isset($this->url[0]) && in_array($this->url[0], $this->prefix))
-			{
-				//Se tiver, o armazena como prefixo da requisição e re arruma o array $this->url
-				$this->prefix = array(
-							'namespace' => $this->url[0].'\\',
-							'path' => $this->url[0].'/'
-				);
-				unset($this->url[0]);//Destroi o primeiro index
-				$this->url = array_values($this->url);//Reorganiza os indexes do array de urls
-			}
-			else
-			{
-				//Se não, o aramazena como vazio
-				$this->prefix = array(
-							'namespace' => '',
-							'path' => ''
-				);
-			}
-			//Testa se tem um prefixo
 		}
 	}
 ?>
--- index.php
+++ index.php
@@ -1,27 +1,32 @@
 <?php
 	session_start();
 	//Auto loader
-	spl_autoload_register(function ($className){
-		//var_dump($className);
-		$root = str_replace('\\','/',__DIR__);
-		var_dump($root);
-		$file = $root. '/' .$className. '.class.php';
-		//var_dump($file);
-		if(!file_exists($file))
-		{
-			trigger_error("Classe '{$className}' não encontrada", E_USER_ERROR);//Trocar por algo melhor
-		}
-		else
-		{
-			require_once "{$file}";
-		}
-	});
+		spl_autoload_register(function ($className){
+			//var_dump($className);
+			$root = str_replace('\\','/',__DIR__);
+			//var_dump($root);
+			$file = $root. '/' .$className. '.class.php';
+			//var_dump($file);
+			if(!file_exists($file))
+			{
+				trigger_error("Classe '{$className}' não encontrada", E_USER_ERROR);//Trocar por algo melhor
+			}
+			else
+			{
+				require_once "{$file}";
+			}
+		});
 	//Auto loader
 	
 	//Erro e exceção handler
-	set_error_handler('App\Core\Error::errorHandler');
-	set_exception_handler('App\Core\Error::exceptionHandler');
-	date_default_timezone_set('America/Sao_Paulo');
+		set_error_handler('App\Core\Error::errorHandler');
+		set_exception_handler('App\Core\Error::exceptionHandler');
+		date_default_timezone_set('America/Sao_Paulo');
 	//Router
 	$router = new App\Core\Router();
+	$table = array(
+		array('url' => 'example', 'REQUEST_METHOD' => 'GET', 'controller' => 'Example')
+	);
+	//var_dump($table);
+	$router->dispatch($table);
 ?>
